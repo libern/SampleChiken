e3c2fe2933066cf84b6f432ca47ce37a0dc9c052
Libern <libern@lechebang.com>
2019-06-05 09:37:32 +0800

    Initial Commit

---
+++ SampleChiken/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SampleChiken
+//
+//  Created by Libern on 2019/6/5.
+//  Copyright © 2019 Someline. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SampleChiken/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SampleChiken
+//
+//  Created by Libern on 2019/6/5.
+//  Copyright © 2019 Someline. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
